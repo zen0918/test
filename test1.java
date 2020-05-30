de57ed33894aeaabd114532caccb2b13e5c059df
zen0918 <66123523+zen0918@users.noreply.github.com>
2020-05-30 15:36:35 +0800

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,9 @@
+public static int getAbsAdd(int x, int y) {
+  assert x != Integer.MIN_VALUE;
+  assert y != Integer.MIN_VALUE;
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  assert (absX <= (Integer.MAX_VALUE – absY));
+  return absX + absY;
+}
+ 
